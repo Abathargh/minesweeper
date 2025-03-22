2fbc7596947e0c3f8be6f0062d035772c038ecb2
mar <gim.marcello@gmail.com>
2025-03-22 19:42:25 +0100

    added: basic neighbours algorithm test case.

--- tests/test_game_logic.nim
+++ tests/test_game_logic.nim
@@ -1,4 +1,5 @@
 import ../minesweeper
+import std/sugar
 import unittest
 
 suite "logic":
@@ -60,3 +61,48 @@ suite "logic":
       let p2  = test_case.p2
       let exp = test_case.expected
       check minesweeper.direction(p1.x, p1.y, p2.x, p2.y) == exp
+
+  test "basic neighbours algorithm":
+    type TestCase = object
+      x, y: int
+      exp:  seq[(int, int, bool)]
+
+    var board = Board()
+    board.init(3, 3)
+    board.fields[0][0].bomb = true
+    board.fields[0][1].bomb = true
+    board.fields[1][2].bomb = true
+    board.fields[2][2].bomb = true
+
+    let test_cases = [
+      TestCase(
+        x: 1,
+        y: 1,
+        exp: @[
+          (0, 0, true),
+          (0, 1, true),
+          (0, 2, false),
+          (1, 0, false),
+          (1, 2, true),
+          (2, 0, false),
+          (2, 1, false),
+          (2, 2, true),
+        ])
+    ]
+
+    for test_case in test_cases:
+      let
+        x =   test_case.x
+        y =   test_case.y
+        exp = test_case.exp
+
+      let neigh = collect:
+        for nx, ny, field in board.neighbours(x, y):
+          (nx, ny, field.bomb)
+
+      check exp.len == neigh.len
+
+      for idx, exp_neighbour in exp.pairs:
+        check exp_neighbour[0] == neigh[idx][0]
+        check exp_neighbour[1] == neigh[idx][1]
+        check exp_neighbour[2] == neigh[idx][2]
